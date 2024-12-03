9363854efa83bc46cec51d233bda2806f5198a25
neduhas-exe <deimantetadas@gmail.com>
2024-12-03 17:12:52 +0200

    Update MovementBusinessRuleValidator.cs

--- WarehouseSystem/Application/Validators/MovementBusinessRuleValidator.cs
+++ WarehouseSystem/Application/Validators/MovementBusinessRuleValidator.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using WarehouseSystem.Services.Interfaces;
 using Presentation.DTOs.Products;
 
-namespace WarehouseSystem.Application.Validation.BusinessRules
+namespace WarehouseSystem.Application.Validators
 {
     public class MovementBusinessRuleValidator
     {

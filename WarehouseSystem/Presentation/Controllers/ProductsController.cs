519433293d8f9152d079dfc526c6deeadcad413e
neduhas-exe <deimantetadas@gmail.com>
2024-12-15 22:57:58 +0200

    Update ProductsController.cs

--- WarehouseSystem/Presentation/Controllers/ProductsController.cs
+++ WarehouseSystem/Presentation/Controllers/ProductsController.cs
@@ -1,34 +1,44 @@
-﻿using Application.Services.Interfaces;
+// Presentation/Controllers/CustomersController.cs
+using Application.Services.Interfaces;
 using Domain.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Presentation.Controllers;
-
-//Temporary controller used to test functionality until front end is developed.
-[ApiController]
-[Route("products")]
-public class ProductsController(IProductsService productsService) : Controller
+namespace Presentation.Controllers
 {
-    private readonly IProductsService _productsService = productsService;
-
-    [HttpGet("{id}")]
-    public IActionResult Get(long id)
+    [ApiController]
+    [Route("customers")]
+    public class CustomersController(ICustomersService customersService) : Controller
     {
-        var product = _productsService.Get(id);
-        return Ok(product);
-    }
+        private readonly ICustomersService _customersService = customersService;
 
-    [HttpGet()]
-    public IActionResult List()
-    {
-        var products = _productsService.List();
-        return Ok(products);
-    }
+        /// <summary>
+        /// Gauti klientą pagal ID
+        /// </summary>
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+            var customer = _customersService.Get(id);
+            return Ok(customer);
+        }
 
-    [HttpPost()]
-    public IActionResult Create(Product product)
-    {
-        var newProduct = _productsService.Create(product);
-        return Ok(newProduct);
+        /// <summary>
+        /// Gauti visų klientų sąrašą
+        /// </summary>
+        [HttpGet()]
+        public IActionResult List()
+        {
+            var customers = _customersService.List();
+            return Ok(customers);
+        }
+
+        /// <summary>
+        /// Sukurti naują klientą
+        /// </summary>
+        [HttpPost()]
+        public IActionResult Create(Customer customer)
+        {
+            var newCustomer = _customersService.Create(customer);
+            return Ok(newCustomer);
+        }
     }
 }

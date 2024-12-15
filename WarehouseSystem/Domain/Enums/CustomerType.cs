2a66eec484e34b569a963cd867ad706b5fe7cd70
neduhas-exe <deimantetadas@gmail.com>
2024-12-15 20:13:49 +0200

    Update Enums

---
+++ WarehouseSystem/Domain/Enums/CustomerType.cs
@@ -0,0 +1,15 @@
+// Domain/Enums/CustomerType.cs
+namespace Domain.Enums;
+
+public enum CustomerType
+{
+    FizinisAsmuo = 0,
+    MazojiBendrija = 1,
+    Ukininkas = 2,
+    ViesojiIstaiga = 3,
+    UAB = 4,
+    IndividualiVeikla = 5,
+    MB = 6,
+    AB = 7
+}
+

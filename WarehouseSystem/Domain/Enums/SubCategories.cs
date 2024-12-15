d6364c34967581d1ee81e61f93b6d2dca3351d45
neduhas-exe <deimantetadas@gmail.com>
2024-12-15 19:45:35 +0200

    Update SubCategories.cs

--- WarehouseSystem/Domain/Enums/SubCategories.cs
+++ WarehouseSystem/Domain/Enums/SubCategories.cs
@@ -1,6 +1,13 @@
-﻿//TODO: Fill in.
-namespace Domain.Enums
+namespace Domain.Enums;
+
+public enum SubCategories
 {
-    public enum SubCategories
-    {
-    }
+    // Kabeliai subcategories
+    ElektrosKabeliai = 0,
+    TinkloKabeliai = 1,
+    OptikoKabeliai = 2,
+    KoaksialinisKabeliai = 3,
+    ValdymoKabeliai = 4,
+    
+    // Add more subcategories as needed
+}

17ff642fb55b8ec43b8470084f0880ca38678c3f
neduhas-exe <deimantetadas@gmail.com>
2024-12-16 11:30:21 +0200

    Create User.cs

---
+++ WarehouseSystem/Domain/Models/User.cs
@@ -0,0 +1,42 @@
+// Domain/Models/User.cs
+namespace Domain.Models;
+
+public class User
+{
+    public long Id { get; set; }
+    
+    /// <summary>
+    /// Vartotojo prisijungimo vardas
+    /// </summary>
+    public string UserName { get; set; }
+    
+    /// <summary>
+    /// Vartotojo vardas
+    /// </summary>
+    public string FirstName { get; set; }
+    
+    /// <summary>
+    /// Vartotojo pavardė
+    /// </summary>
+    public string LastName { get; set; }
+    
+    /// <summary>
+    /// Vartotojo el. paštas
+    /// </summary>
+    public string Email { get; set; }
+    
+    /// <summary>
+    /// Ar vartotojas aktyvus
+    /// </summary>
+    public bool IsActive { get; set; } = true;
+    
+    /// <summary>
+    /// Vartotojo sukūrimo data
+    /// </summary>
+    public DateTime CreatedDate { get; set; }
+    
+    /// <summary>
+    /// Paskutinio prisijungimo data
+    /// </summary>
+    public DateTime? LastLoginDate { get; set; }
+}

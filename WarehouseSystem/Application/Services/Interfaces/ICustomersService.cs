a542cea3d43adb8b8a4e29ac072e6d7632df5900
neduhas-exe <deimantetadas@gmail.com>
2024-12-16 10:31:06 +0200

    Update ICustomersService.cs

--- WarehouseSystem/Application/Services/Interfaces/ICustomersService.cs
+++ WarehouseSystem/Application/Services/Interfaces/ICustomersService.cs
@@ -1,7 +1,30 @@
-// Application/Services/Interfaces/ICustomersService.cs
-public interface ICustomersService
+// Path: WarehouseSystem/Application/Services/Interfaces/ICustomersService.cs
+
+namespace Application.Services.Interfaces
 {
-    Customer Get(long id);
-    List<Customer> List();
-    Customer Create(Customer customer);
+    /// <summary>
+    /// Sąsaja apibrėžianti klientų serviso funkcionalumą
+    /// </summary>
+    public interface ICustomersService
+    {
+        /// <summary>
+        /// Gauti klientą pagal ID
+        /// </summary>
+        /// <param name="id">Kliento ID</param>
+        /// <returns>Kliento objektas</returns>
+        Customer Get(long id);
+
+        /// <summary>
+        /// Gauti visų klientų sąrašą
+        /// </summary>
+        /// <returns>Klientų sąrašas</returns>
+        List<Customer> List();
+
+        /// <summary>
+        /// Sukurti naują klientą
+        /// </summary>
+        /// <param name="customer">Naujo kliento duomenys</param>
+        /// <returns>Sukurtas kliento objektas</returns>
+        Customer Create(Customer customer);
+    }
 }

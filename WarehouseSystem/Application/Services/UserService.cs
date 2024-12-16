af2306c0b84f9a73d0dbf2df65643b65299a6f2d
neduhas-exe <deimantetadas@gmail.com>
2024-12-16 11:34:36 +0200

    Create UserService.cs

---
+++ WarehouseSystem/Application/Services/UserService.cs
@@ -0,0 +1,36 @@
+// Application/Services/UserService.cs
+public class UserService : IUserService
+{
+    private readonly IUserRepository _userRepository;
+    
+    public UserService(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+    
+    public User GetById(long id)
+    {
+        return _userRepository.GetById(id);
+    }
+    
+    public User GetByUsername(string username)
+    {
+        return _userRepository.GetByUsername(username);
+    }
+    
+    public List<User> List()
+    {
+        return _userRepository.List();
+    }
+    
+    public User Create(User user)
+    {
+        user.CreatedDate = DateTime.UtcNow;
+        return _userRepository.Create(user);
+    }
+    
+    public User Update(User user)
+    {
+        return _userRepository.Update(user);
+    }
+}
